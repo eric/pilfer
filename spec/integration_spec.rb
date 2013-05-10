a6728438770c329788774e1789ecd52d0bd5114f
Larry Marburger <larry@marburger.cc>
2013-05-10 14:31:33 -0400

    Stub Pilfer::Server integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,22 +2,30 @@ require 'helper'
 require 'stringio'
 require 'pilfer/logger'
 require 'pilfer/profiler'
+require 'pilfer/server'
 
 describe Pilfer do
-  it 'integrates' do
-    output   = StringIO.new
-    reporter = Pilfer::Logger.new(output)
-    Pilfer::Profiler.new(reporter).
-      profile_files_matching(/integration_spec\.rb/, "testing") do
-        10.times do
-          sleep 0.01
+  context 'reporting to a Pilfer::Logger' do
+    let(:reporter) { Pilfer::Logger.new(output) }
+    let(:output)   { StringIO.new }
+
+    it 'reports profile' do
+      Pilfer::Profiler.new(reporter).
+        profile_files_matching(/integration_spec\.rb/, "testing") do
+          10.times do
+            sleep 0.01
+          end
         end
-      end
 
-    lines = output.string.split("\n")
-    lines[0].should =~ %r{Profile start="[\d-]{10} [\d:]{8} UTC" description="testing"$}
+      lines = output.string.split("\n")
+      lines[0].should =~ %r{Profile start="[\d-]{10} [\d:]{8} UTC" description="testing"$}
 
-    wall_time = lines[1].match(/wall_time=([\d\.]+)/)[1].to_f
-    wall_time.should be_within(10).of(105)
+      wall_time = lines[1].match(/wall_time=([\d\.]+)/)[1].to_f
+      wall_time.should be_within(10).of(105)
+    end
+  end
+
+  context 'reporting to a Pilfer::Server' do
+    it 'reports profile'
   end
 end
